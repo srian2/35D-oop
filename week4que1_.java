1a2a8ddf77abfbbd20f1a4bd511df73db623f29f
srian2 <sandeepkandel45@gmail.com>
2024-05-20 12:20:21 +0545

    week4 que 1

---
+++ week4que1_.java
@@ -0,0 +1,13 @@
+import java.util.Scanner; 
+public class week4que1_ {
+    // 1. Write a program that asks your name and prints “Hello your name” five times. Use a loop.
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter your name: ");
+            String name = scanner.nextLine();
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hello " + name);
+            }
+        }
+    } 
+}
